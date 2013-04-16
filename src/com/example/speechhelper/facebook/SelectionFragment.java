69cbe4a039acffdca5dbafa4d3c473fbc6b2abb2
Jiashi Cao <caojiashi@gmail.com>
2013-04-15 22:09:09 -0700

    step 2d

--- src/com/example/speechhelper/facebook/SelectionFragment.java
+++ src/com/example/speechhelper/facebook/SelectionFragment.java
@@ -1,5 +1,7 @@
 package com.example.speechhelper.facebook;
 
+import java.util.List;
+
 import com.example.speechhelper.R;
 import com.facebook.Request;
 import com.facebook.Response;
@@ -9,12 +11,15 @@ import com.facebook.UiLifecycleHelper;
 import com.facebook.model.GraphUser;
 import com.facebook.widget.ProfilePictureView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SelectionFragment extends Fragment {
@@ -115,6 +120,50 @@ public class SelectionFragment extends Fragment {
 	public void onDestroy() {
 	    super.onDestroy();
 	    uiHelper.onDestroy();
+	}
+	private class ActionListAdapter extends ArrayAdapter<BaseListElement> {
+	    private List<BaseListElement> listElements;
+
+	    public ActionListAdapter(Context context, int resourceId, 
+	                             List<BaseListElement> listElements) {
+	        super(context, resourceId, listElements);
+	        this.listElements = listElements;
+	        // Set up as an observer for list item changes to
+	        // refresh the view.
+	        for (int i = 0; i < listElements.size(); i++) {
+	            listElements.get(i).setAdapter(this);
+	        }
+	    }
+
+	    @Override
+	    public View getView(int position, View convertView, ViewGroup parent) {
+	        View view = convertView;
+	        if (view == null) {
+	            LayoutInflater inflater =
+	                    (LayoutInflater) getActivity()
+	                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	            view = inflater.inflate(R.layout.listitem, null);
+	        }
+
+	        BaseListElement listElement = listElements.get(position);
+	        if (listElement != null) {
+	            view.setOnClickListener(listElement.getOnClickListener());
+	            ImageView icon = (ImageView) view.findViewById(R.id.icon);
+	            TextView text1 = (TextView) view.findViewById(R.id.text1);
+	            TextView text2 = (TextView) view.findViewById(R.id.text2);
+	            if (icon != null) {
+	                icon.setImageDrawable(listElement.getIcon());
+	            }
+	            if (text1 != null) {
+	                text1.setText(listElement.getText1());
+	            }
+	            if (text2 != null) {
+	                text2.setText(listElement.getText2());
+	            }
+	        }
+	        return view;
+	    }
+
 	}
 	
 }
